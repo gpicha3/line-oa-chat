b8ca7ca9554b598c8208d8769eb35d1370b3ea1b
gpicha <pichaya.game@odds.team>
2026-03-02 13:28:46 +0700

    Test webhook use Redis

--- src/app/api/get-reply/route.ts
+++ src/app/api/get-reply/route.ts
@@ -1,13 +1,21 @@
-import { kv } from '@vercel/kv';
+import Redis from 'ioredis';
 import { NextResponse } from 'next/server';
+
+const redis = new Redis(process.env.REDIS_URL || "");
+
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
-    const lastMsg = await kv.get('last_message');
+    const data = await redis.get('last_message');
     
-    return NextResponse.json(lastMsg || { text: "" });
-  } catch (error) {
-    console.error("KV Error:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    if (!data) {
+      return NextResponse.json({ text: "" });
+    }
+
+    return NextResponse.json(JSON.parse(data));
+  } catch (error: any) {
+    console.error("Redis Error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
