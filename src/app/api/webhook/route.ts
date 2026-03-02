92be936f97070d2718f4c1cd677d77881f61054a
gpicha <pichaya.game@odds.team>
2026-03-02 13:29:20 +0700

    Test webhook use Redis

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,20 +1,25 @@
-export const dynamic = 'force-dynamic';
-import { kv } from '@vercel/kv';
+import Redis from 'ioredis';
 import { NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
-  const body = await req.json();
-  const events = body.events;
+const redis = new Redis(process.env.REDIS_URL || "");
 
-  for (const event of events) {
-    if (event.type === 'message' && event.message.type === 'text') {
-      const replyText = event.message.text;
-      
-      await kv.set('last_message', {
-        text: replyText,
-        timestamp: Date.now()
-      });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const events = body.events;
+
+    for (const event of events) {
+      if (event.type === 'message' && event.message.type === 'text') {
+        const replyText = event.message.text;
+        
+        await redis.set('last_message', JSON.stringify({
+          text: replyText,
+          timestamp: Date.now()
+        }));
+      }
     }
+    return NextResponse.json({ status: 'ok' });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
-  return NextResponse.json({ status: 'ok' });
 }
